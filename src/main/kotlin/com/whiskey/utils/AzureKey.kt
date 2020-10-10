b2eb72e12965452fdb750ba4eb9ca2bc7015e202
Lee-WonJun <dldnjs1013@gmail.com>
2020-10-11 04:57:10 +0900

    키 변경 (노출된적이 있어서 Azure 상에서 키 재발급 받음)

--- src/main/kotlin/com/whiskey/utils/AzureKey.kt
+++ src/main/kotlin/com/whiskey/utils/AzureKey.kt
@@ -1,9 +1,9 @@
 package com.whiskey.utils
 
 object AzureKey {
-    val videoIndexerKey = "KIKI" //
+    val videoIndexerKey = "VideoKey" 
 
-    val StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=meetupmedia;AccountKey=Yu5oT+Eo4gk0DWPO1gENKwV/KlHv148Jt1lVQa8YK+t9/YOK+3MvjuNIIZcRln0IEbinZR90EnHySC9Ngbx1rQ==;EndpointSuffix=core.windows.net"
+    val StorageConnectionString = "StorageKey"
 
     const val StorageContainerName = "meetup-media"
 
