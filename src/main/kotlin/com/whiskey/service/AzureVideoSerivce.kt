3c934f76f3c78c755b447092125adb5a172db937
JHyunB <whdgus4892@gmail.com>
2020-10-11 02:46:14 +0900

    Add getIdByExternalId

--- src/main/kotlin/com/whiskey/service/AzureVideoSerivce.kt
+++ src/main/kotlin/com/whiskey/service/AzureVideoSerivce.kt
@@ -2,65 +2,83 @@ package com.whiskey.service
 
 import com.whiskey.client.MeetubeHttpClient
 import com.whiskey.utils.AzureKey
+import org.apache.http.HttpEntity
+import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.entity.ContentType
+import org.apache.http.entity.mime.HttpMultipartMode
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
-import org.apache.http.entity.mime.MultipartEntityBuilder
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.util.UUID
+import java.util.*
+import java.net.URLEncoder
+
 
 @Service
 class AzureVideoSerivce {
 
-    companion object {
-        var accountId = AzureKey.videoIndexerUserId
+    companion object{
+        var accountId = AzureKey.videoIndexerUserId;
         var location = "trial"; // replace with the account's location, or with “trial” if this is a trial account
-        val apiKey = AzureKey.videoIndexerKey
-        var apiUrl = "https://api.videoindexer.ai/$location/Accounts/$accountId"
-        val header = mapOf(
-            "x-ms-client-request-id" to "",
+        val apiKey = AzureKey.videoIndexerKey;
+        var apiUrl = "https://api.videoindexer.ai/${location}/Accounts/${accountId}";
+
+        val header = mapOf<String,String>(
+            "x-ms-client-request-id" to  "",
             "Ocp-Apim-Subscription-Key" to apiKey
         )
+
     }
     private val httpClient: MeetubeHttpClient = MeetubeHttpClient()
 
     fun fileUpload(file: MultipartFile) {
-
+        val uuid = UUID.randomUUID().toString()
         getAccessToken()?.let {
-            val parameter = mapOf(
-                "name" to UUID.randomUUID().toString(),
+            val parameter = mapOf<String,String>(
+                "name" to uuid,
                 "accessToken" to it,
                 "description" to "description",
                 "privacy" to "public",
-                "callbackUrl" to ""
+                "externalId" to uuid
             )
             val builder: MultipartEntityBuilder = MultipartEntityBuilder.create()
 
-            builder.addBinaryBody("file", file.bytes, ContentType.APPLICATION_OCTET_STREAM, file.originalFilename)
+            builder.addBinaryBody("file", file.bytes, ContentType.APPLICATION_OCTET_STREAM,file.originalFilename)
             val body = builder.build()
 
-            val a = httpClient.post("$apiUrl/Videos", parameter, header, body)
+            val a = httpClient.post("${apiUrl}/Videos",parameter, header,body)
             val b = a.content.let { getStreamToString(it) }
-            print(b)
+
+        }
+        getAccessToken()?.let {
+            val parameter = mapOf<String,String>(
+                "externalId" to uuid,
+                "accessToken" to it
+            )
+            val a = httpClient.get("${apiUrl}/Videos/GetIdByExternalId",parameter, header)?.entity?.content
+            println(a?.let (::getStreamToString))
         }
     }
 
     fun getAccessToken(): String? {
-        val parameter = mapOf(
+        val parameter = mapOf<String,String>(
             "allowEdit" to "true"
         )
-        val authUri = "https://api.videoindexer.ai/Auth/$location/Accounts/$accountId/AccessToken"
-        val content = httpClient.get(authUri, parameter, header)?.entity?.content
+        val authUri = "https://api.videoindexer.ai/Auth/${location}/Accounts/${accountId}/AccessToken"
+        val content =  httpClient.get(authUri,parameter, header)?.entity?.content
 
-        return content?.let(::getStreamToString)
+
+        return content?.let (::getStreamToString)
+
     }
 
-    fun getStreamToString(content: InputStream): String {
-        val isr = InputStreamReader(content)
-        val rd = BufferedReader(isr)
+    fun getStreamToString(content:InputStream) :String{
+        val isr = InputStreamReader(content);
+        val rd = BufferedReader(isr);
 
-        return rd.readLine().trim().replace("\"", "")
+        return  rd.readLine().trim().replace("\"","")
     }
+
 }
